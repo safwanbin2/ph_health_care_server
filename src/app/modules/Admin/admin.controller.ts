07ff90b84840ac5326f945632afccba2ebbd0043
safwanbin2 <safwanridwan321@gmail.com>
2024-08-24 23:45:47 +0600

    yet to fix serach bug

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -3,7 +3,7 @@ import { AdminService } from "./admin.service";
 
 const getAllAdmin = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const result = await AdminService.getAllAdmin();
+    const result = await AdminService.getAllAdmin(req?.query);
 
     res.status(200).json({
       success: true,
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -2,8 +2,26 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const getAllAdmin = async () => {
-  const result = await prisma.admin.findMany();
+const getAllAdmin = async (query: any) => {
+  console.log(query);
+  const result = await prisma.admin.findMany({
+    where: {
+      OR: [
+        {
+          name: {
+            contains: query?.searchTerm,
+            mode: "insensitive",
+          },
+        },
+        {
+          email: {
+            contains: query?.searchTerm,
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
+  });
   return result;
 };
 
