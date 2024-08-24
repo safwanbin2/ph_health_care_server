f85bb78cebef178e3424375785d28788a850425a
safwanbin2 <safwanridwan321@gmail.com>
2024-08-24 23:18:48 +0600

    admin

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from "express";
 import cors from "cors";
 import { UserRouter } from "./app/modules/User/user.route";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import { AdminRouter } from "./app/modules/Admin/admin.route";
 
 const app: Application = express();
 
@@ -11,8 +12,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// routes
 app.use("/api/v1/user", UserRouter);
+app.use("/api/v1/admin", AdminRouter);
 
+// error handler
 app.use(globalErrorHandler);
 
 export default app;
---
+++ src/app/modules/Admin/admin.controller.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import { AdminService } from "./admin.service";
+
+const getAllAdmin = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await AdminService.getAllAdmin();
+
+    res.status(200).json({
+      success: true,
+      message: "Admins fetched successfully!",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const AdminController = {
+  getAllAdmin,
+};
