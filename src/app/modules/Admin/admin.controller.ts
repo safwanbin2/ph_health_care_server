493c14ff655a691d2978d7da434000503daa6b80
safwanbin2 <safwanridwan321@gmail.com>
2024-08-26 11:39:30 +0600

    query params opt

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,9 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 import { AdminService } from "./admin.service";
 
+const pickMatchedFilter = (obj, keys) => {
+  const finalObj = {};
+  for (const element of keys) {
+    if (obj && Object.keys(obj).includes(element)) {
+      finalObj[element] = obj[element];
+    }
+  }
+  return finalObj;
+};
+
 const getAllAdmin = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const result = await AdminService.getAllAdmin(req?.query);
+    const filter = pickMatchedFilter(req?.query, [
+      "email",
+      "searchTerm",
+      "contactNumber",
+    ]);
+
+    const result = await AdminService.getAllAdmin(filter);
 
     res.status(200).json({
       success: true,
