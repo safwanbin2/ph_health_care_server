b5809f6e5900fc9e8251a6182878a8f1fb27167f
safwanbin2 <safwanridwan321@gmail.com>
2024-08-28 09:09:16 +0600

    init-1

--- src/app/middlewares/routeNotFound.ts
+++ src/app/middlewares/routeNotFound.ts
@@ -1,14 +1,18 @@
 import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
 
 const routeNotFound = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.status(404).json({
+  res.status(httpStatus.NOT_FOUND).json({
     success: false,
     message: "Route not found!",
-    error: null,
+    error: {
+      path: req?.originalUrl,
+      message: "Your requested path is not found!",
+    },
   });
 };
 
