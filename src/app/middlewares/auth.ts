d6399d329a21af7d6727e785f69d43d760788564
safwanbin2 <safwanridwan321@gmail.com>
2024-08-29 10:48:14 +0600

    type-safety

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,6 +1,7 @@
-import { NextFunction, Request, RequestHandler, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import verifyToken from "../utils/verifyToken";
 import config from "../config";
+import { JwtPayload } from "jsonwebtoken";
 
 const auth = (...roles: string[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
@@ -8,7 +9,10 @@ const auth = (...roles: string[]) => {
       const token = req?.headers?.authorization;
       if (!token) throw new Error("No token!");
 
-      const decoded = verifyToken(token, config.jwt.accessSecret as string);
+      const decoded = verifyToken(
+        token,
+        config.jwt.accessSecret as string
+      ) as JwtPayload;
 
       if (roles?.length && !roles.includes(decoded?.role))
         throw new Error("You are not authorized!");
