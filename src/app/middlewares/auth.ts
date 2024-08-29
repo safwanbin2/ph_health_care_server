25b5871b8bdb23a1e22db2312a9cb6bca95ba927
safwanbin2 <safwanridwan321@gmail.com>
2024-08-29 11:16:21 +0600

    AppError

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -2,12 +2,14 @@ import { NextFunction, Request, Response } from "express";
 import verifyToken from "../utils/verifyToken";
 import config from "../config";
 import { JwtPayload } from "jsonwebtoken";
+import AppEror from "../errors/AppError";
+import httpStatus from "http-status";
 
 const auth = (...roles: string[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       const token = req?.headers?.authorization;
-      if (!token) throw new Error("No token!");
+      if (!token) throw new AppEror(httpStatus.UNAUTHORIZED, "No token!");
 
       const decoded = verifyToken(
         token,
@@ -15,7 +17,7 @@ const auth = (...roles: string[]) => {
       ) as JwtPayload;
 
       if (roles?.length && !roles.includes(decoded?.role))
-        throw new Error("You are not authorized!");
+        throw new AppEror(httpStatus.FORBIDDEN, "You are not authorized!");
 
       next();
     } catch (error) {
