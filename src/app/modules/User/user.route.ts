2105c058000ce2678597317a45f059b6957fcc8c
safwanbin2 <safwanridwan321@gmail.com>
2024-08-30 09:16:24 +0600

    multer setup

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import { UserController } from "./user.controller";
 import auth from "../../middlewares/auth";
 import { UserRole } from "@prisma/client";
+import { fileUploader } from "../../utils/fileUploader";
 
 const router = Router();
 
 router.post(
   "/create-admin",
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  fileUploader.upload.single("file"),
   UserController.createAdmin
 );
 
