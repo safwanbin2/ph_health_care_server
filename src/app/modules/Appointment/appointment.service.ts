96cfa7830a9d58e6fc2832f77d00cb9570fb96a0
safwanbin2 <safwanridwan321@gmail.com>
2024-09-12 12:13:27 +0600

    [route] appointment/relation

--- src/app/modules/Appointment/appointment.service.ts
+++ src/app/modules/Appointment/appointment.service.ts
@@ -26,18 +26,35 @@ const createAppointment = async (user: any, payload: any) => {
 
   const videoCallingId = uuidv4();
 
-  const result = await prisma.appointment.create({
-    data: {
-      patientId: patientData?.id,
-      doctorId: doctorData?.id,
-      scheduleId: payload?.scheduleId,
-      videoCallingId,
-    },
-    include: {
-      patient: true,
-      doctor: true,
-      schedule: true,
-    },
+  const result = await prisma.$transaction(async (tx) => {
+    const appointmentData = await tx.appointment.create({
+      data: {
+        patientId: patientData?.id,
+        doctorId: doctorData?.id,
+        scheduleId: payload?.scheduleId,
+        videoCallingId,
+      },
+      include: {
+        patient: true,
+        doctor: true,
+        schedule: true,
+      },
+    });
+
+    await tx.doctorSchedules.update({
+      where: {
+        doctorId_scheduleId: {
+          doctorId: doctorData?.id,
+          scheduleId: payload?.scheduleId,
+        },
+      },
+      data: {
+        isBooked: true,
+        appointmentId: appointmentData?.id,
+      },
+    });
+
+    return appointmentData;
   });
 
   return result;
