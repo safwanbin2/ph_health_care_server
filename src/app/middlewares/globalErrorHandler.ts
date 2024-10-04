9d891897a5ec98c97d5208b4d2d02b9042699eb1
safwanbin2 <safwanridwan321@gmail.com>
2024-10-04 23:08:25 +0600

    [edit] globalErrorHandler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 
@@ -7,10 +8,24 @@ const globalErrorHandler = async (
   res: Response,
   next: NextFunction
 ) => {
-  res.status(err?.statusCode || httpStatus.INTERNAL_SERVER_ERROR).json({
-    success: false,
-    message: err?.message || err?.name || "Something went wrong!",
-    error: err,
+  let status = err?.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
+  let success = false;
+  let message = err?.message || "Something went wrong!";
+  let error = err;
+
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      message = "Duplicate key error!";
+      error = err.meta;
+    }
+  } else if (err instanceof Prisma.PrismaClientValidationError) {
+    message = "Validation error!";
+  }
+
+  res.status(status).json({
+    success,
+    message,
+    error,
   });
 };
 
