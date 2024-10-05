e5347d6c15ebb9dba0ec6de845d3b5f4d54d7c7b
safwanbin2 <safwanridwan321@gmail.com>
2024-10-05 15:23:22 +0600

    [add] unhandled/uncaught

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,26 @@ async function main() {
   let server: Server = app.listen(config.port, () => {
     console.log("server running on", config.port);
   });
+
+  process.on("uncaughtException", (error) => {
+    console.log(error);
+    if (error) {
+      server.close(() => {
+        console.info("Server closed!");
+      });
+    }
+    process.exit(1);
+  });
+
+  process.on("unhandledRejection", (error) => {
+    console.log(error);
+    if (error) {
+      server.close(() => {
+        console.info("Server closed!");
+      });
+      process.exit(1);
+    }
+  });
 }
 
 main();
